2b372f613b32a161ec52eb1f2a35f852b1230c01
Henry Lee <gbvrcx@gmail.com>
2022-12-22 09:50:39 +0900

    feat: remove unused script

--- script-library/debian-oh-my-bash.sh
+++
@@ -1,6 +0,0 @@
-USE_OMB=${1:-"true"}
-
-if [ "${USE_OMB}" = "true" ] ; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
-    sed -i -e "s/OSH_THEME=.*/OSH_THEME=\"simple\"/g" ~/.bashrc
-fi
