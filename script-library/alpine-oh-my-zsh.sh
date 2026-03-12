8fc71b96a4d58b529b70a3370edb5c0a63088279
Henry Lee <gbvrcx@gmail.com>
2026-03-12 01:36:40 +0000

    Bump up ocaml version
    - Bump up ocaml version to 5.4.0
    - Bump up debian version
    - Remove all images except for debian non-root

--- script-library/alpine-oh-my-zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-USE_OMZ=${1:-"true"}
-
-if [ "${USE_OMZ}" = "true" ] ; then
-    RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"gentoo\"/g" ~/.zshrc
-fi
